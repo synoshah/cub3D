d998d25eab54776e5670d5bfb1fdc165c1be8744
synoshah <haidersyedmustafa43@gmail.com>
2026-08-16 22:51:57 +0400

    saving changes

--- main.c
+++ main.c
@@ -40,9 +40,9 @@ int handle_key(int keycode, t_player *player)
     else if (keycode == KEY_LEFT)
     {
         player->dir_x = (olddir_x * cos(player->rotation_speed) + (olddir_y * sin(player->rotation_speed)));
-        player->dir_y = (olddir_x * sin(player->rotation_speed) + (olddir_y * cos(player->rotation_speed)));
+        player->dir_y = -(olddir_x * sin(player->rotation_speed) + (olddir_y * cos(player->rotation_speed)));
         player->camera_x = (oldcam_x * cos(player->rotation_speed) + (oldcam_y * sin(player->rotation_speed)));
-        player->camera_y = (oldcam_x * sin(player->rotation_speed) + (oldcam_y * cos(player->rotation_speed)));
+        player->camera_y = -(oldcam_x * sin(player->rotation_speed) + (oldcam_y * cos(player->rotation_speed)));
     }
     else if (keycode == KEY_ESC)
     {
@@ -50,6 +50,14 @@ int handle_key(int keycode, t_player *player)
         exit(0);
     }
     return 0;
+}
+
+int rays(int x)
+{
+    int x = 0;
+    while (x <= 800)
+        x++;
+    return x;
 }
 
 int close_game(t_player *player)
@@ -66,12 +74,18 @@ int main(void)
 	t_player 	player;
 	t_data      img;
 
+    player.pypos_x = 5.0; 
+    player.pypos_y = 5.0;
+    player.dir_x = 1.0;
+    player.dir_y = 0.0;
+    player.camera_x = 0.0;
+    player.camera_y = 0.66;
+    player.move_speed = 0.2;
+    player.rotation_speed = 0.05;
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 800, 600, "cub3D");
-
 	img.img = mlx_new_image(mlx, 800, 600);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-
 	draw_hexagon(&img, 50, 50, 0x00FFFF, 50);
 	draw_circle(&img, 100, 100, 0x554053, 100);
 	draw_square(&img, 500, 500, 0x0F325478, 20);
